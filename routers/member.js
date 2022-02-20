d8e1c232b6e0dcb0ad47858873646c8181aff8a2
goldsheep666 <wolf125tw100@gmail.com>
2022-02-20 15:37:53 +0800

    驗證信發送

--- routers/member.js
+++ routers/member.js
@@ -1,33 +1,33 @@
 // 這裏 MemberOrder 的 router
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const connection = require('../utlis/db');
+const connection = require("../utils/db");
 
 // RESTful API 的列表
-router.get('/', async (req, res, next) => {
-  let [data, fields] = await connection.execute('SELECT * FROM order_list');
+router.get("/", async (req, res, next) => {
+  let [data, fields] = await connection.execute("SELECT * FROM order_list");
   console.log(data);
   // res.send ==> 純文字
   // res.render ==> server-side render 會去找樣板
   res.json(data);
 });
 
-router.get('/:id', async (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   // req.params.id
   // req.query.page <- 第幾頁
 
   // 取得目前在第幾頁
   // 如果沒有設定 req.quyer.page，那就設成 1
   let page = req.query.page || 1;
-  console.log('aaaaaaaaa', page);
+  console.log("aaaaaaaaa", page);
 
   // 取得目前的總筆數
   let [total] = await connection.execute(
-    'SELECT COUNT(*) AS total FROM order_list WHERE id=?',
+    "SELECT COUNT(*) AS total FROM order_list WHERE id=?",
     [req.params.id]
   );
-  console.log('bbbbbbb', total); // [ { total: 15 } ]
+  console.log("bbbbbbb", total); // [ { total: 15 } ]
   total = total[0].total; // total = 15
 
   // 計算總共應該要有幾頁
@@ -39,7 +39,7 @@ router.get('/:id', async (req, res, next) => {
   let offset = (page - 1) * perPage;
   // 取得資料
   let [data] = await connection.execute(
-    'SELECT * FROM order_list WHERE id=? ORDER BY date LIMIT ? OFFSET ?',
+    "SELECT * FROM order_list WHERE id=? ORDER BY date LIMIT ? OFFSET ?",
     [req.params.id, perPage, offset]
   );
 
