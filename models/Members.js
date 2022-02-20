ac710df8d3cd26d027789832ab04c722670474cb
Ian Hsieh <energy0702@gmail.com>
2022-02-21 02:58:51 +0800

    MemberOrderDetails撈資料(Accordion.Body bug待修)

--- models/Members.js
+++ models/Members.js
@@ -1,32 +1,54 @@
 // /models/member
+// const { append } = require("express/lib/response");
 const connection = require("../utils/db");
 
-// 取得全部資料
-async function getAll() {
+// 取得MemberOrder資料
+async function getMemberOrder() {
   let [data, fields] = await connection.execute("SELECT * FROM order_list");
+  console.log(data);
+  return data;
+}
+// 取得全部資料
+async function getAll(orderId) {
+  let [data, fields] = await connection.execute(
+    // "SELECT * FROM order_list, order_details, products, member"
+    "SELECT * FROM ((order_details INNER JOIN products ON order_details.product_id = products.product_id) INNER JOIN order_list ON order_details.order_id = order_list.id) INNER JOIN member ON order_list.member_id = member.member_id WHERE order_id=?",
+    // "SELECT * FROM order_details WHERE id=?",
+    [orderId]
+  );
   console.log(data);
   return data;
 }
 
 // 取得某個股票代碼的總筆數
-async function countByCode(member_id) {
-  let [total] = await connection.execute(
-    "SELECT COUNT(*) AS total FROM order_details WHERE member_id=?",
-    [member_id]
-  );
-  return total[0].total;
-}
+// async function countByCode(member_id) {
+//   let [total] = await connection.execute(
+//     "SELECT COUNT(*) AS total FROM order_details WHERE member_id=?",
+//     [member_id]
+//   );
+//   return total[0].total;
+// }
 
-async function getPriceByCode(stockId, perPage, offset) {
-  let [data] = await connection.execute(
-    "SELECT * FROM order_details WHERE member_id=? ORDER BY date LIMIT ? OFFSET ?",
-    [stockId, perPage, offset]
-  );
-  return data;
-}
+// async function getMemberDetailsByCode(member_id, perPage, offset) {
+//   let [data] = await connection.execute(
+//     "SELECT * FROM order_details WHERE member_id=? ORDER BY date LIMIT ? OFFSET ?",
+//     [member_id, perPage, offset]
+//   );
+//   return data;
+// }
+
+// async function getMemberDetailsByCode(member_id) {
+//   let [data] = await connection.execute(
+//     "SELECT * FROM order_details WHERE member_id=? ORDER BY date LIMIT ? OFFSET ?",
+//     // "SELECT * FROM order_details WHERE member_id=?",
+//     [member_id]
+//   );
+//   return data;
+// }
 
 module.exports = {
+  getMemberOrder,
   getAll,
-  countByCode,
-  getPriceByCode,
+  // countByCode,
+  // getMemberDetailsByCode,
 };
--- routers/member.js
+++ routers/member.js
@@ -1,11 +1,27 @@
 // 這裏 member 的 router
 const express = require("express");
 const router = express.Router();
-const memberController = require("../controllers/member");
+// const memberController = require("../controllers/member");
+const connection = require("../utils/db");
 
 // RESTful API 的列表
-router.get("/", memberController.getAll);
+// router.get("/api/member", memberController.getMemberOrder);
 
-router.get("/:stockId", memberController.getPriceByCode);
+// router.get("/api/member/:orderId", memberController.getAll);
+
+router.get("/", async (req, res, next) => {
+  let [data] = await connection.execute("SELECT * FROM order_list");
+  console.log(data);
+  res.json(data);
+});
+
+router.get("/:orderId", async (req, res, next) => {
+  let [data] = await connection.execute(
+    // "SELECT * FROM order_details WHERE id=?",
+    "SELECT * FROM ((order_details INNER JOIN products ON order_details.product_id = products.product_id) INNER JOIN order_list ON order_details.order_id = order_list.id) INNER JOIN member ON order_list.member_id = member.member_id WHERE order_id=?",
+    [req.params.orderId]
+  );
+  res.json(data);
+});
 
 module.exports = router;
