0206e8b4593b3818e1fa7e58771965702bb3013f
Ian Hsieh <energy0702@gmail.com>
2022-02-21 23:44:21 +0800

    MemberOrderDetails撈資料 bug修復

--- models/Members.js
+++ models/Members.js
@@ -2,53 +2,43 @@
 // const { append } = require("express/lib/response");
 const connection = require("../utils/db");
 
+// 取得MemberList全部資料
+async function getMemberList() {
+  let [member] = await connection.execute("SELECT * FROM member");
+  console.log(member);
+  return member;
+}
+
 // 取得MemberOrder資料
-async function getMemberOrder() {
-  let [data, fields] = await connection.execute("SELECT * FROM order_list");
+async function getMemberOrderList() {
+  let [data] = await connection.execute("SELECT * FROM order_list");
   console.log(data);
   return data;
 }
-// 取得全部資料
-async function getAll(orderId) {
-  let [data, fields] = await connection.execute(
-    // "SELECT * FROM order_list, order_details, products, member"
+
+// 取得Member資料
+async function getMember(memberId) {
+  let [member] = await connection.execute(
+    "SELECT * FROM member WHERE member_id=?",
+    [memberId]
+  );
+  console.log(member);
+  return member;
+}
+
+// 取得OrderList全部資料
+async function getMemberOrder(orderId) {
+  let [data] = await connection.execute(
     "SELECT * FROM ((order_details INNER JOIN products ON order_details.product_id = products.product_id) INNER JOIN order_list ON order_details.order_id = order_list.id) INNER JOIN member ON order_list.member_id = member.member_id WHERE order_id=?",
-    // "SELECT * FROM order_details WHERE id=?",
     [orderId]
   );
   console.log(data);
   return data;
 }
 
-// 取得某個股票代碼的總筆數
-// async function countByCode(member_id) {
-//   let [total] = await connection.execute(
-//     "SELECT COUNT(*) AS total FROM order_details WHERE member_id=?",
-//     [member_id]
-//   );
-//   return total[0].total;
-// }
-
-// async function getMemberDetailsByCode(member_id, perPage, offset) {
-//   let [data] = await connection.execute(
-//     "SELECT * FROM order_details WHERE member_id=? ORDER BY date LIMIT ? OFFSET ?",
-//     [member_id, perPage, offset]
-//   );
-//   return data;
-// }
-
-// async function getMemberDetailsByCode(member_id) {
-//   let [data] = await connection.execute(
-//     "SELECT * FROM order_details WHERE member_id=? ORDER BY date LIMIT ? OFFSET ?",
-//     // "SELECT * FROM order_details WHERE member_id=?",
-//     [member_id]
-//   );
-//   return data;
-// }
-
 module.exports = {
+  getMemberList,
+  getMemberOrderList,
+  getMember,
   getMemberOrder,
-  getAll,
-  // countByCode,
-  // getMemberDetailsByCode,
 };
--- routers/member.js
+++ routers/member.js
@@ -5,22 +5,37 @@ const router = express.Router();
 const connection = require("../utils/db");
 
 // RESTful API 的列表
-// router.get("/api/member", memberController.getMemberOrder);
-
-// router.get("/api/member/:orderId", memberController.getAll);
-
+// router.get("/api/member", memberController.getMemberList);
 router.get("/", async (req, res, next) => {
+  let [member] = await connection.execute("SELECT * FROM member");
+  console.log(member);
+  res.json(member);
+});
+
+// router.get("/api/member/member-order", memberController.getMemberOrderList);
+router.get("/member-order", async (req, res, next) => {
   let [data] = await connection.execute("SELECT * FROM order_list");
   console.log(data);
   res.json(data);
 });
 
-router.get("/:orderId", async (req, res, next) => {
+// router.get("/api/member/:memberId", memberController.getMember);
+router.get("/:memberId", async (req, res, next) => {
+  let [member] = await connection.execute(
+    "SELECT * FROM member WHERE member_id=?",
+    [req.params.memberId]
+  );
+  res.json(member);
+});
+
+// router.get("/api/member/member-order/:orderId", memberController.getMemberOrder);
+router.get("/member-order/:orderId", async (req, res, next) => {
   let [data] = await connection.execute(
     // "SELECT * FROM order_details WHERE id=?",
     "SELECT * FROM ((order_details INNER JOIN products ON order_details.product_id = products.product_id) INNER JOIN order_list ON order_details.order_id = order_list.id) INNER JOIN member ON order_list.member_id = member.member_id WHERE order_id=?",
     [req.params.orderId]
   );
+
   res.json(data);
 });
 
