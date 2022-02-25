de67d0ef85151e2ba20fe57b8eb9060ea3dc18f1
Alice Yen <gp40424@gmail.com>
2022-02-26 02:41:32 +0800

    products：剔除checkLogin(白屏)、結帳後端

--- routers/products.js
+++ routers/products.js
@@ -23,4 +23,27 @@ router.get("/:product_group", async (req, res, next) => {
   res.json(data);
 });
 
+// 前端傳送訂單到後端
+router.post("/cart", async (req, res, next) => {
+  let [result] = await connection.execute(
+    "INSERT INTO order_list (member_id,amount,payment,payment_status,delivery,receiver,receiver_phone,address,convenient_store,status,order_time) VALUES (?,?,?,?,?,?,?,?,?,?,?)",
+    [
+      req.body.member_id,
+      req.body.amount,
+      req.body.payment,
+      req.body.payment_status,
+      req.body.delivery,
+      req.body.receiver,
+      req.body.receiverPhone,
+      req.body.address,
+      req.body.convenient_store,
+      req.body.status,
+      req.body.order_time,
+    ]
+  );
+  console.log(result);
+
+  res.json({ message: "ok" });
+});
+
 module.exports = router;
