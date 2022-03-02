2d8f51acdc46a6887547ebfeeeae9b058bb679c8
monday-blue <tflying77@gmail.com>
2022-03-02 15:04:40 +0800

    註解掉home.js裡的console.log

--- routers/home.js
+++ routers/home.js
@@ -479,7 +479,7 @@ router.get("/card-info-home", async (req, res, next) => {
         "SELECT info_no, info_cat_id, big_title, author, big_img, create_time, one_text_one, info_cat FROM information LEFT JOIN info_cat_id ON information.info_cat_id = info_cat_id.id ORDER BY information.create_time DESC LIMIT ?", [4]
     );
 
-    console.log(data);
+    // console.log(data);
     res.json(data);
 });
 
